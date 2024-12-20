f70750a3098c2911f0874f9963406f80a1e9b3d6
ShinYoung-Kim <lukey0515@naver.com>
2024-12-20 15:34:41 +0900

    feat: node에 props 부여

--- renderer.js
+++ renderer.js
@@ -2,6 +2,12 @@ const render = (element, container) => {
 	const isElementText = typeof element === "string";
 	const node = isElementText ? document.createTextNode(element) : document.createElement(element.type);
 
+	Object.keys(element.props)
+		.filter((key) => key !== "children")
+		.forEach((key) => {
+			node[key] = element.props[key];
+		});
+
 	props.children.forEach((child) => {
 		render(child, node);
 	});
