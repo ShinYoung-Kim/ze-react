d32e8ac94b1211e0811ea796f9edb3cc22ea4b2b
ShinYoung-Kim <lukey0515@naver.com>
2024-12-20 17:18:39 +0900

    style: test 코드 분리

---
+++ test.js
@@ -0,0 +1,40 @@
+import renderer from "./renderer.js";
+import core from "./core.js";
+
+const functionalComponent = (props, children) => {
+	return core.createElement(
+		"span",
+		{
+			style: "color: blue; font-size: 16px;",
+			...props,
+		},
+		children
+	);
+};
+
+export const createDefaultElement = () =>
+	renderer.render(
+		core.createElement(
+			"h1",
+			{
+				style: "color: red; font-size: 20px;",
+			},
+			"Hello, ze-react!"
+		),
+		document.getElementById("app")
+	);
+
+export const createFunctionalElement = () =>
+	renderer.render(core.createElement(functionalComponent, null, "Welcome!"), document.getElementById("app"));
+
+export const createNestedFunctionalElement = () =>
+	renderer.render(
+		core.createElement(
+			functionalComponent,
+			{
+				style: "color: green; font-size: 12px;",
+			},
+			core.createElement(functionalComponent, null, "hihi")
+		),
+		document.getElementById("app")
+	);
