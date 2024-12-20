28bbf258eb50f1f6a738c858c2eadfa519cec2c2
ShinYoung-Kim <lukey0515@naver.com>
2024-12-20 15:53:53 +0900

    feat: render 함수 내부 조건부 node get 함수 분리

--- renderer.js
+++ renderer.js
@@ -1,16 +1,27 @@
+const isElementText = (element) => typeof element === "string";
+
+const getNode = (element) => {
+	if (isElementText(element)) {
+		return document.createTextNode(element);
+	}
+
+	return document.createElement(element.type);
+};
+
 const render = (element, container) => {
-	const isElementText = typeof element === "string";
-	const node = isElementText ? document.createTextNode(element) : document.createElement(element.type);
+	const node = getNode(element);
 
-	Object.keys(element.props)
-		.filter((key) => key !== "children")
-		.forEach((key) => {
-			node[key] = element.props[key];
+	if (element.props) {
+		Object.keys(element.props)
+			.filter((key) => key !== "children")
+			.forEach((key) => {
+				node[key] = element.props[key];
+			});
+
+		element.props.children.forEach((child) => {
+			render(child, node);
 		});
-
-	props.children.forEach((child) => {
-		render(child, node);
-	});
+	}
 
 	container.appendChild(node);
 };
