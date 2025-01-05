d47aff9fca68b472ce573527fe5f88319d7f98f8
ShinYoung-Kim <lukey0515@naver.com>
2025-01-06 02:53:57 +0900

    feat: workLoop 생성

--- scheduler.js
+++ scheduler.js
@@ -1,4 +1,11 @@
+import { performUnitOfWork } from "./reconciler.js";
+
+export let nextUnitOfWork = null;
+
 const workLoop = () => {
+	while (nextUnitOfWork) {
+		nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+	}
 	requestIdleCallback(workLoop);
 };
 
