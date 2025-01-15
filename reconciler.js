3f89f82ae54c69c341c92ec7c1a0c727db15a5e9
ShinYoung-Kim <lukey0515@naver.com>
2025-01-15 23:34:36 +0900

    feat: reconcile - fiber update 로직 추가

--- reconciler.js
+++ reconciler.js
@@ -8,24 +8,7 @@ export const performUnitOfWork = (fiber) => {
 
 	// fiber 자식-형제 관계 설정
 	const elements = fiber.props.children;
-	let prevSibling = null;
-
-	elements.forEach((element, index) => {
-		const newFiber = {
-			dom: null,
-			parent: fiber,
-			props: element.props,
-			type: element.type,
-		};
-
-		if (index === 0) {
-			fiber.child = newFiber;
-		} else {
-			prevSibling.sibling = newFiber;
-		}
-
-		prevSibling = newFiber;
-	});
+	reconcileChildren(fiber, elements);
 
 	// 자식 -> 형제 -> 부모 순으로 탐색
 	if (fiber.child) {
@@ -41,6 +24,30 @@ export const performUnitOfWork = (fiber) => {
 	}
 };
 
+const reconcileChildren = (wipFiber, elements) => {
+	let index = 0;
+	let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
+	let prevSibling = null;
+
+	while (index < elements.length || oldFiber) {
+		const element = elements[index];
+		let newFiber = null;
+
+		const sameType = oldFiber && element && element.type === oldFiber.type;
+
+		if (sameType) {
+			newFiber = {
+				type: oldFiber.type,
+				props: element.props,
+				dom: oldFiber.dom,
+				parent: wipFiber,
+				alternate: oldFiber,
+				effectTag: "UPDATE",
+			};
+		}
+	}
+};
+
 export const commitRoot = () => {
 	commitWork(wipRoot.child);
 	currentRoot = wipRoot;
@@ -53,7 +60,9 @@ export const commitWork = (fiber) => {
 	}
 
 	const parentDom = fiber.parent.dom;
-	parentDom.appendChild(fiber.dom);
+	if (fiber.effectTag === "UPDATE" && fiber.dom) {
+		updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+	}
 
 	commitWork(fiber.child);
 	commitWork(fiber.sibling);
